51d5993b191533721444f52a75a8b54e08c370f2
kaio marques <alexandremarqueskaio@gmail.com>
2024-01-03 20:54:43 -0300

    Add API endpoint and update dependencies

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("hi");
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req: any, res: any) => {
+  res.send("Hello, File Upload Service!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
